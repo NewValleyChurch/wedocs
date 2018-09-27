f0ff7b0153f9dbf02381b552866b30134f11c11b
Jon Griffith <jon@jongriffith.com>
2018-09-27 11:28:36 -0700

    update

--- templates/blog-page.php
+++ templates/blog-page.php
@@ -9,8 +9,7 @@ get_header(); ?>
     <div id="content" role="main">
 
       <?php while ( have_posts() ) : the_post(); ?>
-        <?php get_template_part( 'content', 'page' ); ?>
-        <?php comments_template( ’, true ); ?>
+       hello
       <?php endwhile; // end of the loop. ?>
 
     </div><!-- #content -->
