ba1365ec64b71f39d2bbab25781e52822f349ca3
Jon Griffith <jon@jongriffith.com>
2018-09-27 11:26:35 -0700

    update

--- templates/blog-page.php
+++ templates/blog-page.php
@@ -3,43 +3,18 @@
 Template Name: Blog Page
 */
 
- get_header(); ?>
-	<div id="content">
+get_header(); ?>
 
-        <?php query_posts('post_type=post&post_status=publish&posts_per_page=10&paged='. get_query_var('paged')); ?>
+  <div id="primary" class="site-content">
+    <div id="content" role="main">
 
-	<?php if( have_posts() ): ?>
+      <?php while ( have_posts() ) : the_post(); ?>
+        <?php get_template_part( 'content', 'page' ); ?>
+        <?php comments_template( ’, true ); ?>
+      <?php endwhile; // end of the loop. ?>
 
-        <?php while( have_posts() ): the_post(); ?>
+    </div><!-- #content -->
+  </div><!-- #primary -->
 
-	    <div id="post-<?php get_the_ID(); ?>" <?php post_class(); ?>>
-
-        	<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( array(200,220) ); ?></a>
-
-                <h2>taco<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-                <span class="meta"><?php author_profile_avatar_link(48); ?> <strong><?php the_time('F jS, Y'); ?></strong> / <strong><?php the_author_link(); ?></strong> / <span class="comments"><?php comments_popup_link(__('0 comments','example'),__('1 comment','example'),__('% comments','example')); ?></span></span>
-
-		<?php the_excerpt(__('Continue reading »','example')); ?>
-
-            </div><!-- /#post-<?php get_the_ID(); ?> -->
-
-        <?php endwhile; ?>
-
-		<div class="navigation">
-			<span class="newer"><?php previous_posts_link(__('« Newer','example')) ?></span> <span class="older"><?php next_posts_link(__('Older »','example')) ?></span>
-		</div><!-- /.navigation -->
-
-	<?php else: ?>
-
-		<div id="post-404" class="noposts">
-
-		    <p><?php _e('None found.','example'); ?></p>
-
-	    </div><!-- /#post-404 -->
-
-	<?php endif; wp_reset_query(); ?>
-
-	</div><!-- /#content -->
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
