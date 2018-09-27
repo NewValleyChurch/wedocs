d762071b832f2e1e293b37781bde1243b86f9375
Jon Griffith <jon@jongriffith.com>
2018-09-27 11:33:09 -0700

    update

--- templates/blog-page.php
+++ templates/blog-page.php
@@ -8,9 +8,25 @@ get_header(); ?>
   <div id="primary" class="site-content">
     <div id="content" role="main">
 
-      <?php while ( have_posts() ) : the_post(); ?>
-       hello
-      <?php endwhile; // end of the loop. ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<!-- Display the Title as a link to the Post's permalink. -->
+
+ 	<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+
+ 	<!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
+
+ 	<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
+
+ 	<div class="entry">
+ 		<?php the_content(); ?>
+ 	</div>
+
+<?php enwhile; else :
+       // end of the loop. ?>
+
+       <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+   <?php endif; ?>
 
     </div><!-- #content -->
   </div><!-- #primary -->
